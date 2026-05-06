05db65513d98fa1e5e6e58a9dfeceb8f5fd2a585
Volodymyr09-shybk <volodasibka@gmail.com>
2026-05-06 15:00:50 +0200

    etap2

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
- document.getElementById("save").addEventListener("click", function () {
-    const title = document.getElementById("title").value.trim();
-    const desc = document.getElementById("desc").value.trim();
+document.getElementById("save").addEventListener("click", function () {
+    const titleInput = document.getElementById("title");
+    const descInput = document.getElementById("desc");
+
+    const title = titleInput.value.trim();
+    const desc = descInput.value.trim();
 
     if (title === "" || desc === "") return;
 
     const now = new Date();
     const date = now.toLocaleDateString("pl-PL");
 
-    document.getElementById("output").innerHTML = `
-        <p>Opish zadanie: ${title}</p>
-        <p>Data: ${date}</p>
+    const task = document.createElement("div");
+    task.className = "task";
+
+    task.innerHTML = `
+        <p><strong>Zadanie:</strong> ${title}</p>
+        <p>${desc}</p>
+        <p><strong>Data:</strong> ${date}</p>
     `;
+
+    document.getElementById("output").appendChild(task);
+
+    // очищення форми
+    titleInput.value = "";
+    descInput.value = "";
 });
